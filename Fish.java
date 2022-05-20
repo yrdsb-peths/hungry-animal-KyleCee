06175b85c1fc672edbfca16654c3f47a1ae561ef
KyleCee <341138659@gapps.yrdsb.ca>
2022-05-20 09:44:20 -0400

    Fish turns left and right

--- Fish.java
+++ Fish.java
@@ -9,34 +9,68 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Fish extends Actor
 {
     GreenfootSound fishSound = new GreenfootSound("fishbubbling.mp3");
-    GreenfootImage[] idle = new GreenfootImage[6];
+    GreenfootImage[] idleRight = new GreenfootImage[6];
+    GreenfootImage[] idleLeft = new GreenfootImage[6];
     
+    
+    String facing = "right";
+    SimpleTimer animationTimer = new SimpleTimer();
     public Fish()
     {
-        for (int i = 1; i < 6; i++)
+        for (int i = 1; i < idleRight.length; i++)
         {
-            idle[i] = new GreenfootImage("images/idleani/idle" + i + ".png");
+            idleRight[i] = new GreenfootImage("images/idleani/idle" + i + ".png");
+            idleRight[i].scale(110,80);
         }
-        setImage(idle[1]);
-    }
     
+    
+        for (int i = 1; i < idleLeft.length; i++)
+        {
+            idleLeft[i] = new GreenfootImage("images/idleani/idle" + i + ".png");
+            idleLeft[i].mirrorHorizontally();       
+            idleLeft[i].scale(110,80);
+            
+        }
+        
+        
+        animationTimer.mark();
+        setImage(idleRight[1]);
+    }
     
     int imageIndex = 1;
     public void animate()
     {
-        setImage(idle[imageIndex]);
-        imageIndex = (imageIndex + 1) % idle.length;
+        if(animationTimer.millisElapsed() < 200)
+        {
+            return;
+        }
+        animationTimer.mark();
+        
+        if(facing.equals("right"))
+        {
+            setImage(idleRight[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleRight.length;
+        }
+        
+        else
+        {
+            setImage(idleLeft[imageIndex]);
+            imageIndex = (imageIndex + 1) % idleLeft.length;
+        }
     }
+    
     public void act()
     {
         if(Greenfoot.isKeyDown("d"))
         {
             move(4);
+            facing = "right";
         }
         
         if (Greenfoot.isKeyDown("a"))
         {
             move(-4);
+            facing = "left";
         }
         
         if (Greenfoot.isKeyDown("s"))
